b2a018f92014df21655a1646f71b60768e511a28
gidbecxa <gidbec4yeshua@gmail.com>
2024-04-27 11:03:13 +0100

    Added company auth middleware to previous commit

--- Routes/apiRouterV1/company.routes.js
+++ Routes/apiRouterV1/company.routes.js
@@ -14,24 +14,24 @@ module.exports = (apiRouter) => {
     apiRouter.route('/company/auth/verify/').post(ValidatorMiddlewares(companyVerifySchema), companyCtrl.verify);
     apiRouter.route('/company/auth/login/').post(ValidatorMiddlewares(companyLoginSchema), companyCtrl.login);
     apiRouter.route('/company/auth/refresh_endpoint').post(refreshTokenCtrl.refreshToken);
-    
+
     // get routes
     apiRouter.route('/company/getAll/').get(companyCtrl.getAll);
     apiRouter.route('/company/me').get(companyCtrl.getMe);
     apiRouter.route('/company/getOne/:id').get(companyCtrl.getOne);
-    
+
     apiRouter.route('/company/getLogo/:id').get(companyCtrl.getLogo);
-    
+
     // put routes
     // apiRouter.route('/company/update/profileLogo/:id').put(authMiddleware, upload.single('image'), companyCtrl.updatePicture);
-    apiRouter.route('/company/update/logo-description/').put(upload.single('image'), companyCtrl.updateProfilLastData);
+    apiRouter.route('/company/update/logo-description/').put(companyMiddleware, upload.single('image'), companyCtrl.updateProfilLastData);
     apiRouter.route('/company/update/profile').put(ValidatorMiddlewares(updateCompanySchema), companyMiddleware, companyCtrl.updateProfile);
 
     apiRouter.route('/company/update/first-step/').put(ValidatorMiddlewares(updateCompanySchema), companyMiddleware, companyCtrl.updateFirstProfileData);
     apiRouter.route('/company/update/coins/:id').put(authMiddleware, companyCtrl.updateSolde);
-    
+
     // post routes
     apiRouter.route('/company/subscribe/').post(ValidatorMiddlewares(companySubscriptionSchema), companyMiddleware, companyCtrl.subscribeCompany);
-    apiRouter.route('/company/unSubscribe/').post( companyMiddleware, companyCtrl.unSubscribeCompany);
+    apiRouter.route('/company/unSubscribe/').post(companyMiddleware, companyCtrl.unSubscribeCompany);
     apiRouter.route('/company/logo/presigned-url').post(companyCtrl.getLogoWithUrl);
 };
