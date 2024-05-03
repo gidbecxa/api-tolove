662360a12dde50170506249996ab61b008a449ec
gidbecxa <gidbec4yeshua@gmail.com>
2024-05-03 18:27:27 +0100

    Include category of annonces

--- Controllers/carte.controllers.js
+++ Controllers/carte.controllers.js
@@ -24,6 +24,7 @@ module.exports = {
                     image: true,
                     description: true,
                     isAvailable: true,
+                    category: true,
                     /* company: {
                         select: {
                             id: true,
