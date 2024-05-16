3954c9171a68a3456db6d8870d06278f3049b6fe
gidbecxa <gidbec4yeshua@gmail.com>
2024-05-16 17:01:07 +0100

    Using test verify function

--- Routes/apiRouterV1/auth.routes.js
+++ Routes/apiRouterV1/auth.routes.js
@@ -12,7 +12,7 @@ module.exports = (apiRouter) => {
     apiRouter.route('/auth/register/').post(ValidatorMiddlewares(registerSchema), authCtrl.register); // à changer plus tard à registerWithTwilio
     apiRouter.route('/auth/add-user/:agentId').post(upload.single('image'), authCtrl.createUserByAgent);
     apiRouter.route('/auth/add-agent/:agentId').post(authCtrl.createAgent);
-    apiRouter.route('/auth/verify/').post(ValidatorMiddlewares(verifySchema), authCtrl.verifyViaTwilio);
+    apiRouter.route('/auth/verify/').post(ValidatorMiddlewares(verifySchema), authCtrl.verify); // à changer plus tard à verifyViaTwilio
     apiRouter.route('/auth/verify/company/').post(ValidatorMiddlewares(verifySchema), authCtrl.verifyCompanyNoTwilio); // à changer plus tard à verifyCompany
     apiRouter.route('/auth/login/').post(ValidatorMiddlewares(loginSchema), authCtrl.login);
     apiRouter.route('/auth/login-admin/').post(ValidatorMiddlewares(loginAdminSchema), authCtrl.loginAdmin);
