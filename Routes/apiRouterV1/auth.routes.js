a74bb3ba075e6b6d42142e0a8ff1c202f27968d3
gidbecxa <gidbec4yeshua@gmail.com>
2024-05-18 14:48:53 +0100

    add signin route

--- Routes/apiRouterV1/auth.routes.js
+++ Routes/apiRouterV1/auth.routes.js
@@ -10,7 +10,7 @@ const { upload } = require('../../multerConfig');
 module.exports = (apiRouter) => {
 
     apiRouter.route('/auth/register/').post(ValidatorMiddlewares(registerSchema), authCtrl.register); // à changer plus tard à registerWithTwilio
-    apiRouter.route('/auth/register/').post(ValidatorMiddlewares(registerSchema), authCtrl.sendSigninCode); // à mettre à plus tard pour utiliser Twilio
+    apiRouter.route('/auth/signin/').post(ValidatorMiddlewares(registerSchema), authCtrl.sendSigninCode); // à mettre à plus tard pour utiliser Twilio
     apiRouter.route('/auth/add-user/:agentId').post(upload.single('image'), authCtrl.createUserByAgent);
     apiRouter.route('/auth/add-agent/:agentId').post(authCtrl.createAgent);
     apiRouter.route('/auth/verify/').post(ValidatorMiddlewares(verifySchema), authCtrl.verify); // à changer plus tard à verifyViaTwilio
